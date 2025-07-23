a91314862bba7e145ea18a1c7832b5549bc5f16c
ayuarista <aristaadewi8@gmail.com>
2025-07-23 16:20:03 +0800

    add AboutMe component with personal introduction

---
+++ src/app/section/about/about-me.tsx
@@ -0,0 +1,16 @@
+export default function AboutMe() {
+    return (
+        <section className="mx-8 lg:mx-42 mt-7">
+            <div className="text-left">
+                <h1 className="text-3xl">About <b>Me</b></h1>
+                <p className="border-t-4 border-indigo-500 w-12 mt-1"></p>
+            </div>
+            <p className="text-gray-500 text-[16px] text-pretty text-justify mt-6">
+                As a dedicated eleventh-grade student in Software Engineering at Vocational High School, I am passionate about web development and actively expanding my technical skills in front-end technologies. Currently building a strong foundation in web development, I have gained practical experience with core technologies including HTML, CSS, JS, React.js, and Tailwind CSS.
+            </p>
+            <p className="text-gray-500 text-[16px] text-pretty text-justify mt-2">
+            My journey in web development is fueled by a passion for technology and a desire to create intuitive digital experiences. As a junior developer, I am actively learning and exploring front-end technologies, with a strong motivation to transform creative ideas into functional, user-friendly web interfaces.
+            </p>
+        </section>
+    )
+}
