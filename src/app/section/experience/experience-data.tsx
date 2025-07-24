cfe8521e8a1413f8c1e9e2cdb9b7988d4e15e1d5
ayuarista <aristaadewi8@gmail.com>
2025-07-24 16:13:03 +0800

    data experience

---
+++ src/app/section/experience/experience-data.tsx
@@ -0,0 +1,55 @@
+
+export const data = [
+    {
+        id: 1,
+        title: "Winning 2nd Website design at Timedoor",
+        description: "Worked as a software engineering intern, focusing on web development and backend services.",
+        dates: "June 2023 - August 2023",
+        image: "/timedoor.png",
+            
+    },
+    {
+        id: 2,
+        title: "Finalist UI Design at Primakara University",
+        initial: "P",
+        description: "Worked as a software engineering intern, focusing on web development and backend services.",
+        dates: "June 2023 - August 2023",
+        image: "/primakara.png",
+    },
+    {
+        id: 3,
+        initial: "A",
+        title: "Participated Internship at PT.ASANKA",
+        description: "During my internship, I learned a lot.These experiences helped me grow both technically and creatively, and strengthened my interest in front-end development.",
+        dates: "26 December 2024 - 13 Maret 2025"
+    },
+    {
+        id: 4,
+        title: "Winning Favorite Web Design at IndoApps",
+        description: "Worked as a software engineering intern, focusing on web development and backend services.",
+        dates: "June 2023 - August 2023",
+        image: "/indoapps.jpg",
+    },
+    {
+        id: 5,
+        initial: "T",
+        title: "Participated in UI/UX Bootcamp held by Telkom Digiup",
+        description: "I joined the bootcamp organized by Telkom Digiup. By participating in this bootcamp, I increased my UI/UX Design skills. Among 15,000 nationwide applicants, I was selected as one of 1,000 participants.",
+        dates: "01 June 2024 - 07 June 2024",
+        image: "/telkom.png",
+    },
+    {
+        id: 6,
+        title: "Students of Software Engineering major at SMK Negeri 1 Denpasar school",
+        description: "As a dedicated eleventh-grade student in Software Engineering at Vocational High School, I am passionate about web development and actively expanding my technical skills in front-end technologies.",
+        dates: "Study year 2023-2026",
+        image: "/skensa.png",
+    },
+    {
+        id: 7,
+        title: "Participated in more 4+ Web Design Competitions",
+        initial: "4+",
+        description: "As an aspiring front-end developer, I have actively participated in multiple web design competitions, leveraging these opportunities to enhance my technical skills and design proficiency.",
+        dates: "2023 - Now"
+    }
+]
