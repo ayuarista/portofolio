e89f762913bff2fc7ae20385990c5a70c3caa51a
ayuarista <aristaadewi8@gmail.com>
2025-07-27 20:06:45 +0800

    add data projects

---
+++ src/app/section/projects/data.tsx
@@ -0,0 +1,66 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
+export const data = [
+    {
+        id: 1,
+        title: "Develop AI Agent for personal Finance",
+        image: "/web.jpg",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Github",
+                href: "hello"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Develop AI Agent for personal Finance",
+        image: "/web.jpg",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Github",
+                href: "hello"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Develop AI Agent for personal Finance",
+        image: "/web.jpg",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Github",
+                href: "hello"
+            }
+        ]
+    }
+]
