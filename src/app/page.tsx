3de5bee7ca246f4f26add4a9e79430f5534c18f0
ayuarista <aristaadewi8@gmail.com>
2025-07-29 15:31:32 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
   return (
     <section className="flex items-center min-h-screen">
       <div className="w-full">
-        <section id="home" className="flex flex-col-reverse min-h-0 lg:min-h-[90vh] lg:flex-row justify-center items-center mx-4 gap-12">
+        <section className="flex flex-col-reverse min-h-0 lg:min-h-[90vh] lg:flex-row justify-center items-center mx-4 gap-12">
           <HeroHeading />
           <HeroImage />
         </section>
         <AnimatedGrid />
         <AboutMe />
-        <Experience />
-        <Project />
+        <Experience/>
+        <Project/>
       </div>
     </section>
   );
