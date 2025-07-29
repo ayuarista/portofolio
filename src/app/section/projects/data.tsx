ac8e463d2dfd44f1eb9dfd321392a80b3da31378
ayuarista <aristaadewi8@gmail.com>
2025-07-29 15:30:45 +0800

    add data and fix layout

--- src/app/section/projects/data.tsx
+++ src/app/section/projects/data.tsx
@@ -1,30 +1,34 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { TbWorldShare } from "react-icons/tb";
 export const data = [
     {
         id: 1,
-        title: "Develop AI Agent for personal Finance",
-        image: "/web.jpg",
+        title: "Develop Freelancer Find Jobs Website",
+        image: "/project/find-jobs.png",
         dates: "January 2024 - March 2024",
         description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
         tags: [
-            "Python",
-            "Langchain",
-            "OpenAI",
-            "Pandas",
-            "NumPy"
+            "React JS",
+            "Daisy UI",
+            "Tailwind.css",
         ],
         links: [
             {
                 icon: <GitHubLogoIcon />,
-                type: "Github",
+                type: "Source Code",
                 href: "hello"
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://find-job-lite.vercel.app/"
             }
-        ]
+        ],
     },
     {
         id: 2,
         title: "Develop AI Agent for personal Finance",
-        image: "/web.jpg",
+        image: "/project/course-era.png",
         dates: "January 2024 - March 2024",
         description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
         tags: [
@@ -37,15 +41,20 @@ export const data = [
         links: [
             {
                 icon: <GitHubLogoIcon />,
-                type: "Github",
+                type: "Source Code",
                 href: "hello"
-            }
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://course-era.netlify.app/"
+            },
         ]
     },
     {
         id: 3,
         title: "Develop AI Agent for personal Finance",
-        image: "/web.jpg",
+        image: "/project/baliwoosh.png",
         dates: "January 2024 - March 2024",
         description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
         tags: [
@@ -60,7 +69,90 @@ export const data = [
                 icon: <GitHubLogoIcon />,
                 type: "Github",
                 href: "hello"
-            }
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://baliwoosh.netlify.app/"
+            },
         ]
-    }
+    },
+    {
+        id: 4,
+        title: "Develop AI Agent for personal Finance",
+        image: "/project/tkj.png",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Github",
+                href: "hello"
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://web-profile-tkj.netlify.app/"
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: "Develop AI Agent for personal Finance",
+        image: "/project/echo-life.png",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Source Code",
+                href: "hello"
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://echolife.netlify.app/"
+            },
+        ]
+    },
+    {
+        id: 6,
+        title: "Develop AI Agent for personal Finance",
+        image: "/project/nusa-kita.png",
+        dates: "January 2024 - March 2024",
+        description: "Created an AI agent to manage personal finances, including budgeting, expense tracking, and investment recommendations.",
+        tags: [
+            "Python",
+            "Langchain",
+            "OpenAI",
+            "Pandas",
+            "NumPy"
+        ],
+        links: [
+            {
+                icon: <GitHubLogoIcon />,
+                type: "Source Code",
+                href: "hello"
+            },
+            {
+                icon: <TbWorldShare />,
+                type: "Website",
+                href: "https://nusa-kita.netlify.app/"
+            },
+        ]
+    },
 ]
