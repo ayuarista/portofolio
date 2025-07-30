473db82b90745b21fb4755718ec32052826f3bb0
ayuarista <aristaadewi8@gmail.com>
2025-07-30 15:49:06 +0800

    fix style section experience

--- src/app/section/experience/experience.tsx
+++ src/app/section/experience/experience.tsx
@@ -3,16 +3,16 @@ import ExperienceCard from "./experience-card";
 import { data } from "./experience-data";
 export default function Experience() {
     return (
-        <section className="mx-8 lg:mx-36 mt-20" id="experience">
+        <section className="mx-8 lg:mx-36 mt-16" id="experience">
             <div className="text-3xl text-left">
                 <h1 className="inline-flex gap-2"><span className="text-indigo-500"><GoGoal /></span>My <span className="font-bold">Experience</span></h1>
                 <p className="text-gray-500 text-base">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Here are some of the experiences I have gained through my work and projects.
                 </p>
             </div>
 
-            <div className="mt-10">
-            <ul className="mb-4 ml-4 divide-y-2 divide-dashed border-l">
+            <div className="mt-8">
+            <ul className="ml-4 divide-y-2 divide-dashed border-l">
               {data.map((experience, id) => (
                   <ExperienceCard
                     key={id}
