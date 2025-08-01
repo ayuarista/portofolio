ced5653b99d21550f84259f5b467a742c12ea3e7
ayuarista <aristaadewi8@gmail.com>
2025-08-01 09:22:12 +0800

    fix bug img can't find

--- src/app/section/hero/hero-image.tsx
+++ src/app/section/hero/hero-image.tsx
@@ -6,7 +6,7 @@ export default function HeroImage() {
             <div className="relative w-60 md:w-64 lg:w-80 rounded-tl-xl rounded-br-xl p-1 bg-gradient-to-r from-[#7928CA] via-[#0070F3] to-[#38bdf8] shadow-2xl">
                 <div className="rounded-tl-xl rounded-br-xl bg-white dark:bg-black p-1">
                     <Image
-                        src="/me.jpg"
+                        src="/me.JPG"
                         alt="Profile"
                         className="rounded-tl-xl aspect-square object-cover rounded-br-xl"
                         width={320}
