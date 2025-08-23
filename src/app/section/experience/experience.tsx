b750dc740d4c43f4119cd13de7b18c7d1a0b3bd8
ayuarista <aristaadewi8@gmail.com>
2025-08-23 22:44:35 +0800

    increase margin-top in Experience section for improved layout

--- src/app/section/experience/experience.tsx
+++ src/app/section/experience/experience.tsx
@@ -3,7 +3,7 @@ import ExperienceCard from "./experience-card";
 import { data } from "./experience-data";
 export default function Experience() {
     return (
-        <section className="mx-8 lg:mx-36 mt-16" id="experience">
+        <section className="mx-8 lg:mx-36 mt-24" id="experience">
             <div className="text-2xl md:text-3xl text-left">
                 <h1 className="inline-flex gap-2"><span className="text-indigo-500"><GoGoal /></span>My <span className="font-bold">Experience</span></h1>
                 <p className="text-gray-500 text-sm md:text-base">
