1e8ecc116c6418f3f4d82987e5a6fa476a687e81
ayuarista <aristaadewi8@gmail.com>
2025-08-23 22:44:19 +0800

    update AboutMe component with personal introduction and adjust layout

--- src/app/section/about/about-me.tsx
+++ src/app/section/about/about-me.tsx
@@ -14,13 +14,23 @@ export default function AboutMe() {
                         About <b className="font-bold">Me!</b>
                     </span>
                 </div>
-                <MarqueeSkills />
             </div>
-
-
             <section className="mt-5">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-                    <div className="hidden md:block row-span-2 col-start-3 row-start-1 relative">
+
+                    <div className="col-span-3 col-start-1 row-start-1
+                        rounded-xl p-5 text-justify text-sm/6 border-gray-950/[.3] bg-gray-950/[.03] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
+                        <p>
+                            Hello! My name is Ayu Arista, a 12th grade student. Throughout my studies, I have built a solid foundation in HTML, CSS, JavaScript, React.js, and Tailwind CSS, and I am also familiar with using GitHub and Figma for collaboration.
+                        </p>
+                        <p className="mt-2">
+                            I’ve worked on various projects, including UI/UX design, frontend development, and backend development. These experiences, along with participating in competitions, have given me valuable insights into how to turn ideas into functional digital solutions.
+                        </p>
+                        <p className="mt-2">
+                        Looking forward, I aspire to grow as a Junior Developer while continuing to sharpen both my frontend and backend skills.
+                        </p>
+                    </div>
+                    <div className="hidden md:block col-start-3 row-start-2 relative">
                         {/* <div className="absolute -top-4 -right-4 w-full h-full rotate-2 rounded-xl bg-gradient-to-tr from-purple-500 via-indigo-500 to-blue-500 dark:from-indigo-600 dark:via-blue-600 dark:to-cyan-500 -z-10"></div> */}
                         <Image
                             src="/profile6.jpg"
@@ -32,16 +42,8 @@ export default function AboutMe() {
                             blurDataURL="/profile-blur.jpg"
                         />
                     </div>
-
-                    <div className="col-span-2 col-start-1 row-start-1
-                        rounded-xl p-5 text-justify text-sm/6 border-gray-950/[.3] bg-gray-950/[.03] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
-                        <p>
-                        As a dedicated twelve grade student in Software Engineering at Vocational High School, I am passionate about web development and actively expanding my technical skills in front-end technologies. Currently building a strong foundation in web development, I have gained practical experience with core technologies including HTML, CSS, JS, React.js, and Tailwind CSS.
-                        </p>
-                    </div>
-
-                    <div className="col-start-1 row-start-2 rounded-xl ">
-                    <Image
+                    <div className="col-start-2 row-start-2 rounded-xl ">
+                        <Image
                             src="/profile2.jpg"
                             width={330}
                             height={350}
@@ -52,8 +54,8 @@ export default function AboutMe() {
                         />
                     </div>
 
-                    <div className="col-start-2 row-start-2 rounded-xl">
-                         <Image
+                    <div className="col-start-1 row-start-2 rounded-xl">
+                        <Image
                             src="/profile3.jpg"
                             width={330}
                             height={350}
@@ -65,7 +67,7 @@ export default function AboutMe() {
                     </div>
                 </div>
             </section>
-
+            <MarqueeSkills />
         </section>
     )
 }
