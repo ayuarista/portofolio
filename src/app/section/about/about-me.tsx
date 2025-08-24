708c727359421b5727c835204449163f333d92ed
ayuarista <aristaadewi8@gmail.com>
2025-08-24 16:13:35 +0800

    Add skills section with icon and improve layout in AboutMe component

--- src/app/section/about/about-me.tsx
+++ src/app/section/about/about-me.tsx
@@ -1,4 +1,5 @@
 import { MarqueeSkills } from "./marquee-skills"
+import { FaRegFileCode } from "react-icons/fa";
 import { BsFire } from "react-icons/bs";
 import Image from "next/image";
 
@@ -19,15 +20,12 @@ export default function AboutMe() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
 
                     <div className="col-span-3 col-start-1 row-start-1
-                        rounded-xl p-5 text-justify text-sm/6 border-gray-950/[.3] bg-gray-950/[.03] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
-                        <p>
-                            Hello! My name is Ayu Arista, a 12th grade student. Throughout my studies, I have built a solid foundation in HTML, CSS, JavaScript, React.js, and Tailwind CSS, and I am also familiar with using GitHub and Figma for collaboration.
-                        </p>
+                        rounded-xl p-5 text-justify text-sm/6 md:text-[15px]/7 border-gray-950/[.3] bg-gray-950/[.05] hover:bg-gray-950/[.07] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]">
                         <p className="mt-2">
+                            Hello! My name is Ayu Arista, a 12th grade student.
+                            Throughout my studies, I have built a solid foundation in HTML, CSS, JavaScript, React.js, and Tailwind CSS, and I am also familiar with using GitHub and Figma for collaboration. <br />
                             I’ve worked on various projects, including UI/UX design, frontend development, and backend development. These experiences, along with participating in competitions, have given me valuable insights into how to turn ideas into functional digital solutions.
-                        </p>
-                        <p className="mt-2">
-                        Looking forward, I aspire to grow as a Junior Developer while continuing to sharpen both my frontend and backend skills.
+                            Looking forward, I aspire to grow as a Junior Developer while continuing to sharpen both my frontend and backend skills.
                         </p>
                     </div>
                     <div className="hidden md:block col-start-3 row-start-2 relative">
@@ -67,6 +65,16 @@ export default function AboutMe() {
                     </div>
                 </div>
             </section>
+            <div className="mt-12 flex flex-col gap-4">
+                <div className="text-2xl md:text-3xl text-left flex items-center justify-center gap-2 ">
+                    <span className="text-indigo-500 text-3xl">
+                        <FaRegFileCode />
+                    </span>
+                    <span className="">
+                        My <b className="font-bold">Skills</b>
+                    </span>
+                </div>
+            </div>
             <MarqueeSkills />
         </section>
     )
